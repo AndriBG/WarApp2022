b14617437c3346597011045f3a57459439950960
AndriBG14 <guzmanbonilla7@gmail.com>
2022-03-15 19:54:50 -0400

    add modal

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular'
+import { ModalPage } from '../modal/modal.page'
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private modal : ModalController) {}
+
+  async addAction(e){
+    const modal = await this.modal.create({
+      component: ModalPage,
+      // cssClass: 'my-custom-class',
+      breakpoints: [0, 0.25, 0.5, 0.75, 1],
+      keyboardClose: false,
+      // handle: true,
+      // htmlAttributes: ModalAttributes,
+      // leaveAnimation:  (ba: any) => ,
+      componentProps: {
+        // video: e.video,
+        // title: e.titulo,
+        // descripcion: e.descripcion
+      }
+    });
+    return await modal.present();
+  }
 
 }
