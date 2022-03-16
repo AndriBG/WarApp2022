b37759e989b9102812f11ed1ae16c709f769239c
AndriBG <guzmanbonilla7@gmail.com>
2022-03-16 08:54:24 -0400

    camera pluggin added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,140 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Directory, Filesystem } from '@capacitor/filesystem';
+import { LoadingController, Platform } from '@ionic/angular';
+// import { fileURLToPath } from 'url';
+
+const IMAGE_DIR = 'stored-images';
+
+interface LocalFile {
+  name: string,
+  path: string,
+  data: string
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  images: LocalFile[] = [];
+  // loading: any;
 
+  constructor(private platform: Platform, private loadingCtrl: LoadingController) {}
+
+  async ngOnInit() {
+    this.loadFiles();
+  }
+
+  async loadFiles() {
+    this.images = [];
+    const loading = await this.loadingCtrl.create({
+      message: 'Loading Data'
+    });
+
+    await loading.present();
+
+    Filesystem.readdir({
+      directory: Directory.Data,
+      path: IMAGE_DIR
+    }).then( result => {
+      console.log('result', result);
+      this.loadFileData(result.files)
+    }, async err => {
+      console.error('error ', err);
+      await Filesystem.mkdir({
+        directory: Directory.Data,
+        path: IMAGE_DIR
+      });
+    }).then( e => {
+      loading.dismiss();
+    });
+  }
+
+  async loadFileData(fileNames: string[]) {
+    for (let f of fileNames) {
+      const filePath = `${IMAGE_DIR}/${f}`;
+      const readFile = await Filesystem.readFile({
+        directory: Directory.Data,
+        path: filePath
+      });
+
+      this.images.push({
+        name: f,
+        path: filePath,
+        data: `data:image/jpeg;base64,${readFile.data}`
+      });
+      console.log('READ: ', readFile)
+    }
+  }
+
+  async selectImage () {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera
+    });
+    // console.log(image);
+    if(image) {
+      this.saveImage(image);
+    }
+  }
+
+  async saveImage(photo: Photo) : Promise<void> {
+    const base64Data = await this.readAsBase64(photo);
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      directory: Directory.Data,
+      path: `${IMAGE_DIR}/${fileName}`,
+      data: base64Data
+    });
+    this.loadFiles();
+  }
+
+  private async readAsBase64(photo: Photo) {
+    if(this.platform.is('hybrid')){
+      const file = await Filesystem.readFile({
+        path: photo.path
+      });
+      return file.data
+    } else {
+      const response = await fetch(photo.webPath!);
+      const blob = await response.blob();
+
+      return await this.convertBlobToBase64(blob) as string;
+    }
+
+  }
+
+  private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+        resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
+
+  // upload the
+  async startUpload( file: LocalFile ){
+
+  }
+
+  async deleteImage( file: LocalFile ) {
+    await Filesystem.deleteFile({
+      directory: Directory.Data,
+      path: file.path
+    });
+    this.loadFiles();
+  }
+
+    // private async readAsBase64(photo: Photo) {
+    //   const response = await fetch(photo.webPath!);
+    //   const blob = await response.blob();
+
+    //   return await this.convertBlobToBase64(blob) as string;
+    // }
 }
